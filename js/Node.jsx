97e600b37d52cdaabb3488fa6d43039f0bb906b2
Levi Tan Ong <levi@byimplication.com>
2015-08-21 14:45:36 +0800

    adds css

--- js/Node.jsx
+++ js/Node.jsx
@@ -1,9 +1,11 @@
+import NodeStyle from "../css/Node.css";
+
 import React from 'react';
 
 export default class Node extends React.Component {
   render() {
     return (
-      <div>
+      <div className="Node">
         <h4>{ this.props.checked ? "[x]" : "[ ]"} { this.props.title }</h4>
         <div>
           { this.props.children }
