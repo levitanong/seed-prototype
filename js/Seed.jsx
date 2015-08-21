b3032aca078e2b630424d52694175363e8c62083
Levi Tan Ong <levi@byimplication.com>
2015-08-21 14:18:15 +0800

    Node element, sample sample data

--- js/Seed.jsx
+++ js/Seed.jsx
@@ -1,9 +1,56 @@
 import React from 'react';
 
+var data = {
+  "root" : {
+    title: "My Seed",
+    checked: false,
+    notes: null,
+    children: [0, 1]
+  },
+  0: {
+    title: "Finish working on Sakay",
+    checked: false,
+    notes: null,
+    children: [2, 3]
+  },
+  1: {
+    title: "Finish working on Seed",
+    checked: false,
+    notes: null,
+    children: [4, 5]
+  },
+  2: {
+    title: "Build iOS App",
+    checked: false,
+    notes: null,
+    children: null
+  },
+  3: {
+    title: "Build Android App",
+    checked: false,
+    notes: null,
+    children: null
+  },
+  4: {
+    title: "Finish sample data",
+    checked: false,
+    notes: null,
+    children: null
+  },
+  5: {
+    title: "Finish frontend",
+    checked: false,
+    notes: null,
+    children: null
+  }
+}
+
 export default class Seed extends React.Component {
   render() {
     return (
-      <div>derp</div>
+      <div>
+      
+      </div>
     );
   }
 }
