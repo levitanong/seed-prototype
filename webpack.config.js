7d528247832e41d60ead220051768650571e635d
Levi Tan Ong <levi@byimplication.com>
2015-08-21 13:58:07 +0800

    minor config fix

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ module.exports = {
   module: {
     loaders: [
       { test: /\.jsx?$/, exclude: /node_modules/,  loader: "babel-loader" },
-      { test: /\.css$/, loader: "style-loader!css-loader!postcss-loader") },
+      { test: /\.css$/, loader: "style-loader!css-loader!postcss-loader" },
     ]
   }
 }
