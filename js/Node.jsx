2925b1a89927650fba1cc3b5a8b808adc02438e9
Levi Tan Ong <levi@byimplication.com>
2015-08-21 16:20:02 +0800

    nodes can now be deleted

--- js/Node.jsx
+++ js/Node.jsx
@@ -6,14 +6,29 @@ export default class Node extends React.Component {
   constructor(props) {
     super(props);
     this._clickhandler = this._clickhandler.bind(this);
+    this._changeTitleHandler = this._changeTitleHandler.bind(this);
+    this._keyDownHandler = this._keyDownHandler.bind(this);
   }
   _clickhandler() {
-    console.log(this.props.dataID);
+    // console.log(this.props.dataID);
+    this.props.onUpdateChecked(this.props.dataID);
+  }
+  _changeTitleHandler(event) {
+    this.props.onUpdateTitle(this.props.dataID, event.target.value);
+  }
+  _keyDownHandler(event){
+    if (event.key === "Backspace" && this.props.title === "") {
+      // console.log(event.key);
+      this.props.onDeleteNode(this.props.dataID, this.props.parentID);
+      event.preventDefault();
+    }
   }
   render() {
+
     return (
       <div className="Node">
-        <h4 onClick={ this._clickhandler }>{ this.props.checked ? "[x]" : "[ ]"} { this.props.title }</h4>
+        <span onClick={ this._clickhandler }>{ this.props.checked ? "[x]" : "[ ]"}</span>
+        <input value={ this.props.title } onChange={ this._changeTitleHandler } onKeyDown={ this._keyDownHandler } />
         <div>
           { this.props.children }
         </div>
@@ -24,8 +39,15 @@ export default class Node extends React.Component {
 
 Node.defaultProps = {
   dataID: undefined,
+  parentID: null,
   checked: false,
   title: "",
   notes: null,
   children: null
+}
+
+Node.propTypes = {
+  onUpdateChecked: React.PropTypes.func,
+  onUpdateTitle: React.PropTypes.func,
+  onDeleteNode: React.PropTypes.func
 }
--- js/Seed.jsx
+++ js/Seed.jsx
@@ -2,68 +2,115 @@ import SeedStyle from "../css/Seed.css";
 
 import React from 'react';
 import Node from "./Node";
-
-var data = {
-  "root" : {
-    title: "My Seed",
-    checked: false,
-    notes: null,
-    children: [0, 1]
-  },
-  0: {
-    title: "Finish working on Sakay",
-    checked: false,
-    notes: null,
-    children: [2, 3]
-  },
-  1: {
-    title: "Finish working on Seed",
-    checked: false,
-    notes: null,
-    children: [4, 5]
-  },
-  2: {
-    title: "Build iOS App",
-    checked: false,
-    notes: null,
-    children: null
-  },
-  3: {
-    title: "Build Android App",
-    checked: false,
-    notes: null,
-    children: null
-  },
-  4: {
-    title: "Finish sample data",
-    checked: false,
-    notes: null,
-    children: null
-  },
-  5: {
-    title: "Finish frontend",
-    checked: false,
-    notes: null,
-    children: null
-  }
-}
+import D from "datascript";
+import _ from "lodash";
 
 export default class Seed extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: {
+        root : {
+          title: "My Seed",
+          checked: false,
+          notes: null,
+          childNodes: [0, 1]
+        },
+        0: {
+          title: "Finish working on Sakay",
+          checked: false,
+          notes: null,
+          childNodes: [2, 3]
+        },
+        1: {
+          title: "Finish working on Seed",
+          checked: false,
+          notes: null,
+          childNodes: [4, 5]
+        },
+        2: {
+          title: "Build iOS App",
+          checked: false,
+          notes: null,
+          childNodes: []
+        },
+        3: {
+          title: "Build Android App",
+          checked: false,
+          notes: null,
+          childNodes: []
+        },
+        4: {
+          title: "Finish sample data",
+          checked: false,
+          notes: null,
+          childNodes: []
+        },
+        5: {
+          title: "Finish frontend",
+          checked: false,
+          notes: null,
+          childNodes: []
+        }
+      }
+    }
+    this._updateChecked = this._updateChecked.bind(this);
+    this._updateTitle = this._updateTitle.bind(this);
+    this._deleteNode = this._deleteNode.bind(this);
+  }
+  _updateChecked(id) {
+    var node = this.state.data[id];
 
+    if (!node.childNodes.length) {
+      this.setState({data: _.set(this.state.data, id + ".checked", !this.state.data[id].checked)});
+    }
+  }
+  _updateTitle(id, newTitle) {
+    this.setState({data: _.set(this.state.data, id + ".title", newTitle)});
+  }
+  _deleteNode(id, parentId) {
+    var newData = _.chain(this.state.data)
+      .omit(id)
+      .set(parentId + ".childNodes", this.state.data[parentId].childNodes.filter(childNodeKey => childNodeKey !== id))
+      .value();
+    this.setState({data: newData});
+  }
   render() {
-    var renderTree = function(key){
+    var data = this.state.data;
+
+    var recursiveCheck = function(key) {
+      var node = data[key];
+      if (node && node.childNodes && node.childNodes.length) {
+        return _.reduce(node.childNodes, (accumulator, childNodeKey) => {
+          return accumulator && recursiveCheck(childNodeKey);
+        }, true);
+      } else {
+        return node && node.checked;
+      }
+    }
+
+    var renderTree = function(key, parentID){
       var node = data[key];
       return (
-        <Node key={ key } dataID={ key } { ...node }>
-          { node.children && node.children.map(function(child) {
-            return renderTree(child)
+        <Node 
+          key={ key } 
+          dataID={ key } 
+          parentID={ parentID }
+          { ...node } 
+          checked={ recursiveCheck(key) } 
+          onUpdateChecked={ this._updateChecked }
+          onUpdateTitle={ this._updateTitle }
+          onDeleteNode={ this._deleteNode }>
+          { (node && node.childNodes && !!node.childNodes.length) && node.childNodes.map(function(child) {
+            return renderTree(child, key);
           })}
         </Node>
       )
-    }
+    }.bind(this);
+
     return (
       <div>
-        { renderTree("root") }
+        { renderTree("root", null) }
       </div>
     );
   }
